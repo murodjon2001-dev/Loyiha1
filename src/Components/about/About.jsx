88d22a38ebede0b1913eefdd434ec3d7e6bbf2e7
murodjon2001-dev <xmurodjon200@gmail.com>
2025-01-11 21:15:48 +0500

    Update About.jsx

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,5 +1,11 @@
 const About = () => {
-  return <input type="text" placeholder='Typing' />
+  return (
+       <input type="text" placeholder='Typing' />
+       <input type="text" placeholder='Typing' />
+       <input type="text" placeholder='Writing' />
+       <input type="text" placeholder='Typing' />
+       <input type="text" placeholder='Typing' />
+    )
 }
 
 export default  About
